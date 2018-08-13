7edc9eeaa668e01ad3b7925f840b41ffb367148b
Summertime <Summertime@users.noreply.github.com>
2018-08-14 04:45:57 +0930

    Corrrectly add digital ocean

--- builder/providers/digitalocean.py
+++ builder/providers/digitalocean.py
@@ -1,11 +1,8 @@
 import json
 from pathlib import Path
 
-from currency_converter import CurrencyConverter
-c = CurrencyConverter()
-
 def generator():
-    with open(str(Path(__file__).with_name('scaleway.json'))) as f:
+    with open(str(Path(__file__).with_name('digitaloceanjson'))) as f:
         plans = json.load(f)['plans']
     for plan in plans:
         plan = {
