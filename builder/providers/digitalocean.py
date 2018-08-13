05e7715207018e489292224fc180ac5d81ecf9a7
Summertime <Summertime@users.noreply.github.com>
2018-08-14 04:50:39 +0930

    .

--- builder/providers/digitalocean.py
+++ builder/providers/digitalocean.py
@@ -2,7 +2,7 @@ import json
 from pathlib import Path
 
 def generator():
-    with open(str(Path(__file__).with_name('digitaloceanjson'))) as f:
+    with open(str(Path(__file__).with_name('digitalocean.json'))) as f:
         plans = json.load(f)['plans']
     for plan in plans:
         plan = {
