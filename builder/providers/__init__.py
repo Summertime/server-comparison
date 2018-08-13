bd37627207a71f08b715e29ddaa51458bed8d029
Summertime <Summertime@users.noreply.github.com>
2018-08-14 04:48:03 +0930

    Pull DO into the generated set

--- builder/providers/__init__.py
+++ builder/providers/__init__.py
@@ -1,10 +1,12 @@
 from .vultr import generator as vultr
 from .scaleway import generator as scaleway
+from .digitalocean import generator as digitalocean
 
 __all__ = ['generators']
 
 plans = [
     *vultr(),
     *scaleway(),
+    *digitalocean(),
 ]
 plans.sort(key=lambda item:item['cost'])
